2cb65575ba38af796f7223d0d1b75dae53b00835
Mizuki <mizuki@Yota-KnoMacBook-Pro.local>
2026-01-03 21:02:49 +0900

    feat: コンポーネントに入力パラメータを追加

--- src/app/housing-location/housing-location.ts
+++ src/app/housing-location/housing-location.ts
@@ -1,15 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, input } from "@angular/core";
+import { HousingLocationInfo } from "../housinglocation";
 
 @Component({
-  selector: 'app-housing-location',
+  selector: "app-housing-location",
   imports: [],
-  template: `
-    <p>
-      housing-location works!
-    </p>
-  `,
+  template: ` <p>housing-location works!</p> `,
   styles: ``,
 })
 export class HousingLocation {
-
+  housingLocation = input.required<HousingLocationInfo>();
 }
