daac100e16b064344f0f5ba40daf76a28808e99f
Mizuki <mizuki@Yota-KnoMacBook-Pro.local>
2026-01-04 19:07:29 +0900

    feat/add-routing

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,20 +1,23 @@
 import { Component } from "@angular/core";
-import { Home } from "./home/home";
+import { RouterLink, RouterOutlet } from "@angular/router";
+import { RouterModule } from "@angular/router";
 
 @Component({
   selector: "app-root",
-  imports: [Home],
+  imports: [RouterOutlet, RouterLink, RouterModule],
   template: ` <main>
-    <header class="brand-name">
-      <img
-        class="brand-logo"
-        src="/assets/logo.svg"
-        alt="logo"
-        aria-hidden="true"
-      />
-    </header>
+    <a [routerLink]="['/']">
+      <header class="brand-name">
+        <img
+          class="brand-logo"
+          src="/assets/logo.svg"
+          alt="logo"
+          aria-hidden="true"
+        />
+      </header>
+    </a>
     <section class="content">
-      <app-home />
+      <router-outlet />
     </section>
   </main>`,
   styleUrls: ["./app.css"],
